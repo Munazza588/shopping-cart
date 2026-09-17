b842ee7e69f9a7ee63b13afdecdc1cce1c2498b1
Munazza588 <munazzahabib@arizona.edu>
2026-09-17 00:31:11 -0700

    create the input boc for user to type in the quantity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,40 @@
-import './Card.css'
+import './Card.css';
+import { useState } from 'react';
 function Card({title,url,price}) {
+    const [amount,setAmount] = useState(0);
+
+
+    function handleAddButton() {
+        setAmount(amount +1);
+
+    }
+
+    function handleSubButton() {
+        if (amount > 0) {
+            setAmount(amount -1);
+        }
+
+    }
+
+    function handleInputBoxAmount(e) {
+        setAmount(Number(e.target.value));
+    }
+
     return (
         <div className="card-div">
             <img src={url}  />
-            <p className='title-card'>{title}</p>
-            <p className='price'>Price: ${price}</p>
+            <div className='text-of-items'>
+                <p className='title-card'>{title}</p>
+                <p className='price'>Price: ${price}</p>
+            </div>
+            <div className='button-set-div'>
+                <div className='add-items-div'>
+                <button onClick={handleSubButton} className='button-minus'>-</button>
+                <input type="number" value ={amount}  onChange={handleInputBoxAmount}  min="1" className='input-box'/>
+                <button onClick={handleAddButton} className='button-plus'>+</button>
+                </div>
+                <button className='add-to-cart-button'>Add to Cart</button>
+            </div>
         </div>
     )
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,9 @@
+function Cart() {
+    return(
+        <div>
+            <div>
+                <p>Shopping Cart</p>
+            </div>
+        </div>
+    )
+}
